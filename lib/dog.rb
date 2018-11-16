8762fe4b4688d6c642ef387a55f8019115219661
Heather Miller <heather.miller114@gmail.com>
2018-11-16 02:45:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,9 +65,7 @@ class Dog
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.album)
-      @id = DB[:conn].execute("SELECT ")
-    
-
-  
-  
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
 end
